f95614bd3d6e282160d9f07e3afa69b5491ec772
Przemek <przemekandrzejczak@gmail.com>
2020-12-22 20:45:35 +0100

    Zadanie rekrutacyjne

---
+++ main.js
@@ -0,0 +1,41 @@
+const productName = document.querySelector('#name');
+const productAmount = document.querySelector('#amount');
+const productType = document.querySelector('#type');
+const productContainers = document.querySelectorAll('.added-products div');
+const submitBtn = document.querySelector('.submitBtn')
+let index = 0;
+
+
+const products = [];
+
+
+const addProduct = () => {
+    const newProduct = {
+        name: productName.value,
+        quantity: productAmount.value,
+        type: productType.value,
+        id: productType.value
+    }
+
+    const pElement = document.createElement('p');
+    pElement.classList.add('product');
+    pElement.textContent = `${newProduct.name} x ${newProduct.type !== 'meat'? newProduct.quantity: newProduct.quantity + ' kg'}`;
+    pElement.setAttribute('id',`${newProduct.id}`);
+    products.push(pElement);
+    const type = newProduct.type;
+
+
+    productContainers.forEach(productContainer => {
+        if(productContainer.classList.contains(`${type}`)){
+            products.forEach(product => {
+                if(product.id === `${type}`){
+                    productContainer.appendChild(product);
+                }
+            })
+        }
+    })
+
+index++;
+}
+
+submitBtn.addEventListener('click', addProduct);
